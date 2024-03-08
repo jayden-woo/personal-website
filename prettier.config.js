b267b749a5c6778272028c2455941ed6144c9944
jayden-woo <juncheng1301@gmail.com>
2024-03-09 03:44:47 +1100

    Fix config files

--- prettier.config.js
+++ prettier.config.js
@@ -1,17 +1,15 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   plugins: ["prettier-plugin-tailwindcss"],
-  config: {
-    printWidth: 100,
-    tabWidth: 2,
-    useTabs: false,
-    semi: true,
-    singleQuote: false,
-    jsxSingleQuote: false,
-    trailingComma: "es5",
-    bracketSpacing: true,
-    bracketSameLine: false,
-    arrowParens: "always",
-    endOfLine: "lf",
-  },
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
 };
