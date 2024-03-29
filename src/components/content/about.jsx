6263c46e93905272b1339a9fd9c9cb9de759e996
jayden-woo <juncheng1301@gmail.com>
2024-03-30 03:42:22 +1100

    Add responsive design for about section

--- src/components/content/about.jsx
+++ src/components/content/about.jsx
@@ -5,12 +5,14 @@ import { SectionTitle } from "@/components/section";
 
 export default function About() {
   return (
-    <section id={CONTENT_SECTIONS.About} className="mb-32 scroll-mt-16">
-      <SectionTitle>
-        About <span className="inline-block first-letter:text-sky-400">Me</span>
-      </SectionTitle>
+    <section id={CONTENT_SECTIONS.About} className="mb-16 scroll-mt-16 lg:mb-32">
+      <TagWrapper className="my-4 contents" before="<h2>" after="</h2>">
+        <SectionTitle>
+          About <span className="inline-block first-letter:text-sky-400">Me</span>
+        </SectionTitle>
+      </TagWrapper>
       <TagWrapper className="my-4 leading-relaxed" before="<p>" after="</p>">
-        <p className="my-2 opacity-0">
+        <p className="my-2 md:motion-safe:opacity-0">
           Hey there! My name is Jayden and I&apos;m a computing graduate with a Bachelor&apos;s
           degree from the{" "}
           <Link href="https://study.unimelb.edu.au/find/courses/major/computing-and-software-systems/">
@@ -19,19 +21,19 @@ export default function About() {
           . I&apos;ve always been fascinated by tech and machineries since young, and my time at uni
           only deepened that passion further.
         </p>
-        <p className="my-4 opacity-0">
+        <p className="my-4 md:motion-safe:opacity-0">
           During my studies, I dove into everything from algorithms to AI, and I loved every bit of
           it. Some things I&apos;ve dabbled in are web dev, computing systems, software dev, machine
           learning, game dev, and even had the chance to do a whole investments minor paired with
           some accounting.
         </p>
-        <p className="my-4 opacity-0">
+        <p className="my-4 md:motion-safe:opacity-0">
           Now that I&apos;m out in the real world, I&apos;m pumped to put my skills to work. Whether
           it&apos;s building cool software, coding aesthetic website, or tackling big challenges,
           I&apos;m ready to make a dent in the tech world and gain valuable experiences from
           different fields.
         </p>
-        <p className="my-2 opacity-0">
+        <p className="my-2 md:motion-safe:opacity-0">
           {/* TODO: Add free time hobbies section and easter egg for BoTW */}
           In my free time, I love playing some occasional badminton, hanging around beaches or
           parks, running around Hyrule solving <Link href="/">Shrine puzzles</Link>, or even bending
