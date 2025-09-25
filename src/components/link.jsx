90ac79d1a5c7b6d504c79951a1dd1aa53f91f7f8
jayden-woo <juncheng1301@gmail.com>
2025-09-25 21:46:06 +1000

    Update Next version

--- src/components/link.jsx
+++ src/components/link.jsx
@@ -15,13 +15,17 @@ export default function Link({ children, variant = LINK_VARIANTS.Text, className
         className,
         "border-b border-sky-400 font-medium text-slate-200 hover:border-b-2 focus-visible:text-sky-400"
       );
-      // TODO: Clean unused animations
+      // TODO: Choose between 3 animation options for links when hovered
       // Add highlighting hover animation over links
-      // className +=
-      //   " font-medium text-slate-200 relative before:-z-10 before:block before:absolute before:inset-0 before:bg-sky-400 before:transition-transform before:duration-300 before:scale-x-0 before:origin-bottom-right before:hover:scale-x-100 before:hover:origin-bottom-left";
+      // className = clsx(
+      //   className,
+      //   "relative font-medium text-slate-200 before:absolute before:inset-0 before:-z-10 before:block before:origin-bottom-right before:scale-x-0 before:bg-sky-400 before:transition-transform before:duration-300 before:hover:origin-bottom-left before:hover:scale-x-100"
+      // );
       // Add underlining hover animation over links
-      // className +=
-      //   " font-medium text-slate-200 relative before:absolute before:-bottom-0.5 before:h-0.5 before:w-full before:bg-sky-400 before:rounded before:transition-transform before:ease-in before:scale-x-0 before:hover:scale-x-100 before:duration-250";
+      // className = clsx(
+      //   className,
+      //   "before:duration-250 relative font-medium text-slate-200 before:absolute before:-bottom-0.5 before:h-0.5 before:w-full before:scale-x-0 before:rounded before:bg-sky-400 before:transition-transform before:ease-in before:hover:scale-x-100"
+      // );
       break;
     case LINK_VARIANTS.Icon:
       className = clsx(className, "fill-current hover:fill-slate-200 focus-visible:fill-slate-200");
