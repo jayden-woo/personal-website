c577d880524f7e316da7a9da0a838a8f13a2be10
jayden-woo <juncheng1301@gmail.com>
2025-09-25 22:07:01 +1000

    Add skills data with technology logos

---
+++ src/assets/data/skills.jsx
@@ -0,0 +1,127 @@
+import {
+  JavaScript,
+  HTML,
+  CSS,
+  Python,
+  Java,
+  SQL,
+  C,
+  CSharp,
+  React,
+  NextJS,
+  Redux,
+  NodeJS,
+  Express,
+  TailwindCSS,
+  Git,
+  GitHub,
+  MongoDB,
+  MySQL,
+  VisualStudioCode,
+  Postman,
+  Render,
+} from "@/assets/images/skills";
+
+const SKILLS_DATA = [
+  {
+    label: "Languages",
+    items: [
+      {
+        name: "Javascript",
+        Image: JavaScript,
+      },
+      {
+        name: "HTML",
+        Image: HTML,
+      },
+      {
+        name: "CSS",
+        Image: CSS,
+      },
+      {
+        name: "Python",
+        Image: Python,
+      },
+      {
+        name: "Java",
+        Image: Java,
+      },
+      {
+        name: "SQL",
+        Image: SQL,
+      },
+      {
+        name: "C",
+        Image: C,
+      },
+      {
+        name: "C#",
+        Image: CSharp,
+      },
+    ],
+  },
+  {
+    label: "Frameworks",
+    items: [
+      {
+        name: "React",
+        Image: React,
+      },
+      {
+        name: "Next.js",
+        Image: NextJS,
+      },
+      {
+        name: "Redux",
+        Image: Redux,
+      },
+      {
+        name: "Node.js",
+        Image: NodeJS,
+      },
+      {
+        name: "Express",
+        Image: Express,
+      },
+      {
+        name: "Tailwind CSS",
+        Image: TailwindCSS,
+      },
+    ],
+  },
+  {
+    label: "Tools",
+    items: [
+      {
+        name: "Git",
+        Image: Git,
+      },
+      {
+        name: "GitHub",
+        Image: GitHub,
+      },
+      {
+        name: "MongoDB",
+        Image: MongoDB,
+      },
+      {
+        name: "MySQL",
+        Image: MySQL,
+      },
+      {
+        name: "Visual Studio Code",
+        Image: VisualStudioCode,
+      },
+      {
+        name: "Render",
+        Image: Render,
+      },
+      {
+        name: "Postman",
+        Image: Postman,
+      },
+    ],
+  },
+];
+
+export default SKILLS_DATA;
