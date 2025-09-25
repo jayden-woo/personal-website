6dd355d5e34a7d522d944321e85caa6a03136ea6
jayden-woo <juncheng1301@gmail.com>
2025-09-25 22:06:15 +1000

    Add skills section with responsive design

--- src/components/content/skills.jsx
+++ src/components/content/skills.jsx
@@ -1,36 +1,23 @@
 import { CONTENT_SECTIONS } from "@/assets/data/constants";
+import SKILLS_DATA from "@/assets/data/skills";
+import { SectionTitle } from "@/components/section";
+import SkillCategory from "@/components/section/skill-category";
+import TagWrapper from "@/components/tag-wrapper";
 
 export default function Skills() {
   return (
-    <section id={CONTENT_SECTIONS.Skills}>
-      <h2>What Skills I&apos;ve Acquired</h2>
-      <ul>
-        <li>Languages</li>
-        <ul>
-          <li>Javascript</li>
-          <li>Python</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Java</li>
-          <li>SQL</li>
-        </ul>
-        <li>Libraries & Frameworks</li>
-        <ul>
-          <li>React</li>
-          <li>Next</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>Redux</li>
-        </ul>
-        <li>Tools</li>
-        <ul>
-          <li>Git & GitHub</li>
-          <li>Postman</li>
-          <li>NextJS</li>
-          <li>MongoDB</li>
-          <li>MySQL</li>
-        </ul>
-      </ul>
+    <section id={CONTENT_SECTIONS.Skills} className="mb-16 scroll-mt-16 lg:mb-32">
+      <TagWrapper className="my-4 contents" before="<h2>" after="</h2>">
+        <SectionTitle>
+          <span className="hidden md:inline">What </span>
+          <span className="inline-block first-letter:text-sky-400">Skills</span> I&apos;ve Acquired
+        </SectionTitle>
+      </TagWrapper>
+      <TagWrapper className="my-4 leading-relaxed" tag="ul" before="<ul>" after="</ul>">
+        {SKILLS_DATA.map((item, index) => (
+          <SkillCategory key={index} {...item} />
+        ))}
+      </TagWrapper>
     </section>
   );
 }
---
+++ src/components/section/skill-category.jsx
@@ -0,0 +1,22 @@
+export default function SkillCategory({ label, items }) {
+  return (
+    <div className="group my-5 grid grid-cols-3 gap-3 md:grid-cols-5 md:motion-safe:invisible lg:grid-cols-4">
+      <h3 className="col-span-full content-center text-lg md:col-span-2 md:text-center md:text-xl md:font-medium md:duration-300 md:group-hover:text-sky-400 md:motion-safe:invisible md:motion-safe:transition-colors">
+        {label}
+      </h3>
+      <ul className="contents">
+        {items.map(({ name, Image }, index) => (
+          <li key={index} className="group/logo relative size-full md:motion-safe:invisible">
+            <Image className="size-full grayscale duration-300 hover:grayscale-0 motion-safe:transition" />
+            <span
+              role="tooltip"
+              className="absolute bottom-full left-1/2 mb-2 -translate-x-1/2 text-nowrap rounded border border-gray-500/20 bg-gray-870 px-2 py-1 text-xs opacity-0 duration-300 group-hover/logo:opacity-100 motion-safe:transition-opacity"
+            >
+              {name}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
